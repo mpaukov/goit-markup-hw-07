e96dc8d9f5c6fe2c2d3709bec2dff52452428fc5
Michael Paukov <michael.paukov@gmail.com>
2021-11-11 17:38:53 +0100

    add modal

---
+++ js/main.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    formsend: document.querySelector('.js-speaker-form')};
+
+  refs.formsend.addEventListener('submit', e => {
+    e.preventDefault();
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`));
+    e.currentTarget.reset();
+  });
+
+   
+})();
+
+
