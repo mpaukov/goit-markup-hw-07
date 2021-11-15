cb1fae89a9c65cd139f3348003d31aa3c7a49ff4
Michael Paukov <michael.paukov@gmail.com>
2021-11-15 07:41:28 +0100

    add js formating

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
 (() => {
   const refs = {
-    formsend: document.querySelector('.js-speaker-form')};
+    formsend: document.querySelector(".js-speaker-form"),
+  };
 
-  refs.formsend.addEventListener('submit', e => {
+  refs.formsend.addEventListener("submit", (e) => {
     e.preventDefault();
     new FormData(e.currentTarget).forEach((value, name) =>
-      console.log(`${name}: ${value}`));
+      console.log(`${name}: ${value}`)
+    );
     e.currentTarget.reset();
   });
-
-   
 })();
-
-
